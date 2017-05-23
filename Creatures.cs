9ded9fe5cdf00909b4690e66361f4cd5be9c269d
adiko <dikodam@googlemail.com>
2017-05-23 10:07:33 +0200

    Creatures (+ Animal) + Test

---
+++ Creatures.cs
@@ -0,0 +1,21 @@
+﻿namespace GenericPair
+{
+    public abstract class Creature : ILike
+    {
+        public string Name { get; }
+
+        public Creature(string name)
+        {
+            Name = name;
+        }
+
+        public abstract bool Check(ILike you);
+    }
+
+    public abstract class Animal : Creature
+    {
+        public Animal(string name) : base(name)
+        {
+        }
+    }
+}
---
+++ GenericPairNUnitTest/CreaturesTest.cs
@@ -0,0 +1,29 @@
+﻿using GenericPair;
+using NUnit.Framework;
+
+namespace GenericPairNUnitTest
+{
+    [TestFixture]
+    public class CreaturesTest
+    {
+
+        private class SmallCreature : Creature
+        {
+            public SmallCreature(string name) : base(name)
+            {
+            }
+
+            public override bool Check(ILike you)
+            {
+                throw new System.NotImplementedException();
+            }
+        }
+
+        [Test]
+        public void CreatureTest()
+        {
+            SmallCreature tested = new SmallCreature("Horst");
+            Assert.AreEqual("Horst", tested.Name);
+        }
+    }
+}
