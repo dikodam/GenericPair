ef886a4e4bd3ea463acbb0003ee63d3e0db5ab93
adiko <dikodam@googlemail.com>
2017-05-23 10:26:18 +0200

    Creatures Test

--- Creatures.cs
+++ Creatures.cs
@@ -1,4 +1,6 @@
-﻿namespace GenericPair
+﻿using GenericPair;
+
+namespace GenericPair
 {
     public abstract class Creature : ILike
     {
@@ -16,6 +18,54 @@
     {
         public Animal(string name) : base(name)
         {
+        }
+    }
+
+    public class Human : Creature
+    {
+        public Human(string name) : base(name)
+        {
+        }
+
+        public override bool Check(ILike you)
+        {
+            return you is Human || you is Cat;
+        }
+    }
+
+    public class Cat : Animal
+    {
+        public Cat(string name) : base(name)
+        {
+        }
+
+        public override bool Check(ILike you)
+        {
+            return you is Cat;
+        }
+    }
+
+    public class Dog : Animal
+    {
+        public Dog(string name) : base(name)
+        {
+        }
+
+        public override bool Check(ILike you)
+        {
+            return true;
+        }
+    }
+
+    public class Spider : Animal
+    {
+        public Spider(string name) : base(name)
+        {
+        }
+
+        public override bool Check(ILike you)
+        {
+            return false;
         }
     }
 }
--- GenericPairNUnitTest/CreaturesTest.cs
+++ GenericPairNUnitTest/CreaturesTest.cs
@@ -19,11 +19,56 @@ namespace GenericPairNUnitTest
             }
         }
 
+        private Human human = new Human("hooman");
+        private Cat cat = new Cat("gadse");
+        private Dog dog = new Dog("doge");
+        private Spider spider = new Spider("i think i spider");
+
+
         [Test]
         public void CreatureTest()
         {
             SmallCreature tested = new SmallCreature("Horst");
             Assert.AreEqual("Horst", tested.Name);
         }
+
+        public void HumanNameTest()
+        {
+            Human tested = new Human("Fritz");
+            Assert.AreEqual(tested.Name, "Fritz");
+        }
+
+        public void HumanLikeTest()
+        {
+            Assert.IsTrue(human.Check(human));
+            Assert.IsTrue(human.Check(cat));
+            Assert.IsFalse(human.Check(dog));
+            Assert.IsFalse(human.Check(spider));
+        }
+
+        public void CatLikeTest()
+        {
+            Assert.IsTrue(cat.Check(cat));
+            Assert.IsFalse(cat.Check(human));
+            Assert.IsFalse(cat.Check(dog));
+            Assert.IsFalse(cat.Check(spider));
+        }
+
+        public void DogLikeTest()
+        {
+            Assert.IsTrue(dog.Check(dog));
+            Assert.IsTrue(dog.Check(human));
+            Assert.IsTrue(dog.Check(cat));
+            Assert.IsTrue(dog.Check(spider));
+        }
+
+        public void SpiderLikeTest()
+        {
+            Assert.IsFalse(spider.Check(spider));
+            Assert.IsFalse(spider.Check(human));
+            Assert.IsFalse(spider.Check(cat));
+            Assert.IsFalse(spider.Check(dog));
+        }
+
     }
 }
