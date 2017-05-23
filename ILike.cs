898d2ecc8dadaf1c021eaaa73331826409c429d8
adiko <dikodam@googlemail.com>
2017-05-23 20:41:19 +0200

    Kommentare Augenkrebs enjoy

--- ILike.cs
+++ ILike.cs
@@ -1,5 +1,8 @@
 ﻿namespace GenericPair
 {
+    /// <summary>
+    /// prescribes the method <code>bool Check(ILike you)</code> to be implemented
+    /// </summary>
     public interface ILike
     {
         bool Check(ILike you);
--- PairList.cs
+++ PairList.cs
@@ -1,22 +1,54 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 
 // ReSharper disable InconsistentNaming
 namespace GenericPair
 {
+    /// <summary>
+    /// linked list for reference types which implement the <see cref="ILike">ILike</see> interface
+    /// </summary>
+    /// <typeparam name="T1">first reference type of the pair, implements <see cref="ILike">ILike</see></typeparam>
+    /// <typeparam name="T2">second reference type of the pair, implements <see cref="ILike">ILike</see></typeparam>
     public class PairList<T1, T2> : IEnumerable where T1 : class, ILike where T2 : class, ILike
     {
+        #region internal wrapper
+
+        /// <summary>
+        /// internal wrapper class for list elements
+        /// </summary>
         class PairWrapper
         {
+            /// <summary>
+            /// list element
+            /// </summary>
             public Pair<T1, T2> Data { get; }
+
+            /// <summary>
+            /// reference to the next list element. may be null.
+            /// </summary>
             public PairWrapper Next { get; set; }
 
+            /// <summary>
+            /// constructor for the wrapper. puts the given pair object into the wrapper.
+            /// </summary>
+            /// <param name="pair">a pair object to be hold by the wrapper. may not be null.</param>
+            /// <exception cref="ArgumentException">thrown if pair is null</exception>
             public PairWrapper(Pair<T1, T2> pair)
             {
+                if (pair == null)
+                {
+                    throw new ArgumentException("The content of the list may not be null!");
+                }
                 Data = pair;
             }
 
+            /// <summary>
+            /// <returns>if this wrapper object has a reference to a next wrapper object</returns>
+            /// </summary>
             public bool HasNext => Next != null;
         }
+
+        #endregion
 
         private PairWrapper first;
 
@@ -31,8 +63,17 @@ namespace GenericPair
             }
         }
 
+        /// <summary>
+        /// adds the given parameter to the end of the list
+        /// </summary>
+        /// <param name="pairToAdd">the new pair to be added</param>
+        /// <exception cref="ArgumentException">thrown if given pair is null</exception>
         public void Add(Pair<T1, T2> pairToAdd)
         {
+            if (pairToAdd == null)
+            {
+                throw new ArgumentException("pair to be added may not be null");
+            }
             if (first == null)
             {
                 first = new PairWrapper(pairToAdd);
